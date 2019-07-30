44c0d86c8c92b3b40b8774900e56ac502260e640
Rene Pickhardt <rene@rene-pickhardt.de>
2019-07-30 13:43:56 -0400

    fixed leftover path

--- regtest-helper/start.sh
+++ regtest-helper/start.sh
@@ -3,7 +3,7 @@
 #rm -rf /tmp/regtest-env
 mkdir -p /tmp/regtest-env/{btc,ln1,ln2}
 
-bitcoind --regtest --datadir=/tmp/spark-env/btc --rpcuser=rpcuser --rpcpassword=rpcpassword &
+bitcoind --regtest --datadir=/tmp/regtest-env/btc --rpcuser=rpcuser --rpcpassword=rpcpassword &
 sleep 1
 
 source startln1.sh &
